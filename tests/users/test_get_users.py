42db849a14a6c08fd2281188b1dc3c80bf529f71
sem1on <sgorlov21@gmail.com>
2025-05-02 16:27:17 +0300

    add fix

--- tests/users/test_get_users.py
+++ tests/users/test_get_users.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.baseclasses.response import Response
 from src.paidentic_schema.users import User
 
@@ -10,5 +12,11 @@ def test_getting_users_list(get_users):
         .assert_status_code(200) \
         .validate_data(User)
 
-def test_generate_palyer(get_generator_player):
-    print(get_generator_player.build())
+@pytest.mark.parametrize("balance", {
+    100,
+    -21,
+    0,
+    "33"
+})
+def test_generate_palyer(balance, get_generator_player):
+    print(get_generator_player.set_balanse(balance).build())
